ccf4b100061da1e3e19313b3e043c7b2f9e7958c
xig5010 <xig5010@gmail.com>
2014-02-22 21:35:49 -0800

    imp devise for user data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  #required for heroku and set this to actual host.
+  config.action_mailer.default_url_options = { :host => 'sxg-omr-pinteresting.herokuapp.com' }
 end
